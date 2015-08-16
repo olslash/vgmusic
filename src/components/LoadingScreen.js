400bd3a043e6776e547feaf491ef40f3d92590a5
Alan Love <alan@cattes.us>
2015-08-16 13:50:13 -0500

    added error screen and loading component

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoadingScreen extends Component {
+    render() {
+        return (
+            <h1>Loading Tracks</h1>
+        )
+    }
+}
+
+export default LoadingScreen;
