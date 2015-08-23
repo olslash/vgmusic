35421bb398cefdab1dbdadec3483ca06639006c5
Mitch R <olordhux+github@gmail.com>
2015-08-23 13:18:53 -0700

    inject touch events for mui

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('./styles/site.scss');
+require("react-tap-event-plugin")();
 
-import React from 'react';  
+import React from 'react';
 import App from './containers/App';
+
 
 React.render(<App />, document.body);
